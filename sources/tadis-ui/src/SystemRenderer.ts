d5657c52a5ee90d802931f9932e655fa98e3f9b6
xndlnk <xndlnk@gmail.com>
2022-07-25 16:45:31 +0200

    upgraded some more deps

--- sources/tadis-ui/src/SystemRenderer.ts
+++ sources/tadis-ui/src/SystemRenderer.ts
@@ -2,33 +2,41 @@ import * as d3Base from 'd3'
 import { graphviz } from 'd3-graphviz'
 
 import { Node } from './domain/model'
-import { SystemToDotConverter, Options as SystemToDotOptions } from './domain/systemToDot'
+import {
+  SystemToDotConverter,
+  Options as SystemToDotOptions
+} from './domain/systemToDot'
 
 // attach all d3 plugins to the d3 library
 // see https://www.giacomodebidda.com/how-to-import-d3-plugins-with-webpack/
 const d3 = Object.assign(d3Base, { graphviz })
 
 export class SystemRenderer {
-
   public static removeHeightFromSvg() {
-    d3.select('#graph').select('svg').select((d, i, nodes) => {
-      const selectedNode = d3.select(nodes[i])
-      const currentHeight = selectedNode.attr('height')
-      console.log('currentHeight: ' + currentHeight)
-      selectedNode.attr('height', null)
-    })
+    d3.select('#graph')
+      .select('svg')
+      .select((d, i, nodes) => {
+        const selectedNode = d3.select(nodes[i])
+        const currentHeight = selectedNode.attr('height')
+        console.log('currentHeight: ' + currentHeight)
+        selectedNode.attr('height', null)
+      })
   }
 
   private readonly defaultOptions: SystemToDotOptions = {
     urlExtractor: (node: Node) => ''
   }
 
-  renderSystem(system: Node, postRenderActions?: () => void, options?: SystemToDotOptions) {
-    const transition = d3.transition()
-      .delay(100)
-      .duration(1000)
+  renderSystem(
+    system: Node,
+    postRenderActions?: () => void,
+    options?: SystemToDotOptions
+  ) {
+    const transition = d3.transition().delay(100).duration(1000)
 
-    const systemToDotConverter = new SystemToDotConverter(options || this.defaultOptions)
+    const systemToDotConverter = new SystemToDotConverter(
+      options || this.defaultOptions
+    )
 
     d3.select('#graph')
       .graphviz()
@@ -36,7 +44,7 @@ export class SystemRenderer {
       .width(window.innerWidth - 3 * 25)
       .height(window.innerHeight - 200)
       .fit(true)
-      .renderDot(systemToDotConverter.convertSystemToDot(system), function() {
+      .renderDot(systemToDotConverter.convertSystemToDot(system), function () {
         this.resetZoom()
 
         if (postRenderActions) {
@@ -44,5 +52,4 @@ export class SystemRenderer {
         }
       })
   }
-
 }
